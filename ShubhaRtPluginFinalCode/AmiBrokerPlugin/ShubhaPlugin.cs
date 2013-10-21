25320276b56ab848cc4c73d34ece4d137af882da
webshub <info@shubhalabha.in>
2013-10-21 18:50:06 +0530

    Capture symbol and save it listbox

--- ShubhaRtPluginFinalCode/AmiBrokerPlugin/ShubhaPlugin.cs
+++ ShubhaRtPluginFinalCode/AmiBrokerPlugin/ShubhaPlugin.cs
@@ -19,12 +19,31 @@ using System.Collections.Generic;
 using Microsoft.Office.Interop.Excel;
 using System.Timers;
 using System.Configuration;
-using System.IO;
 using System.Net;
 using System.Data.SqlClient;
 using System.Reflection;
 using Microsoft.VisualBasic;
 using Microsoft.Win32;
+using System.Net.Mail;
+using System.Text;
+using System.Globalization;
+using System.Data;
+using System.Threading;
+using System.Windows;
+
+using System.ComponentModel;
+using ManagedWinapi.Windows;
+using ManagedWinapi.Accessibility;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Specialized;
+using System.Collections;
+using System.IO.Compression;
+using System.Diagnostics;
+
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+
+using System.Security.Principal;
 
 namespace AmiBrokerPlugin
 {
@@ -41,6 +60,28 @@ namespace AmiBrokerPlugin
             InitializeComponent();
         }
 
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        public static IntPtr FindChildWindow(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszTitle)
+        {
+            // Try to find a match.
+            IntPtr hwnd = FindWindowEx(hwndParent, IntPtr.Zero, lpszClass, lpszTitle);
+            if (hwnd == IntPtr.Zero)
+            {
+                // Search inside the children.
+                IntPtr hwndChild = FindWindowEx(hwndParent, IntPtr.Zero, null, null);
+                while (hwndChild != IntPtr.Zero && hwnd == IntPtr.Zero)
+                {
+                    hwnd = FindChildWindow(hwndChild, IntPtr.Zero, lpszClass, lpszTitle);
+                    if (hwnd == IntPtr.Zero)
+                    {
+                        // If we didn't find it yet, check the next child.
+                        hwndChild = FindWindowEx(hwndParent, hwndChild, null, null);
+                    }
+                }
+            }
+            return hwnd;
+        }
         public static System.Data.DataTable GetDataTable(string strFileName)
         {
             ADODB.Connection oConn = new ADODB.Connection();
@@ -465,8 +506,102 @@ namespace AmiBrokerPlugin
             System.Diagnostics.Process.Start("http://shubhalabha.in/disclaimer/license/");
         }
 
+
+        public void getsymbol()
+        {
+
+
+
+            //dataGrid3.Items.Add(new DataItem { Column0 = "abcd", Column1 = "dasdasdas" });
+            //dataGrid3.Items.Add(new DataItem { Column0 = "abcasdsasdd", Column1 = "das1111111111111dasdas" });
+            string terminalname = ConfigurationManager.AppSettings["terminalname"];
+            Process[] processes = null;
+            Type type;
+
+           
+                
+                    try
+                    {
+                        type = Type.GetTypeFromProgID("nest.scriprtd");
+
+                        m_server = (IRtdServer)Activator.CreateInstance(type);
+                        processes = Process.GetProcessesByName("NestTrader");
+
+
+                    }
+                    catch
+                    {
+                    MessageBox.Show(" Please start Nest as Run as Administrator and again start Realtime combo");
+                        return;
+                    }
+               
+               
+                   
+                 IntPtr abcd = new IntPtr();
+                IntPtr abcd1 = new IntPtr();
+                IntPtr windowHandle = new IntPtr();
+
+
+
+                List<Thread> processtostartback = new List<Thread>();
+                SystemAccessibleObject sao, f;
+
+                foreach (Process p in processes)
+                {
+                    windowHandle = p.MainWindowHandle;
+
+                    //System.Windows.Forms.MessageBox.Show(p.HandleCount.ToString());
+
+                    abcd = FindChildWindow(windowHandle, IntPtr.Zero, "#32770", "");
+                    abcd1 = FindChildWindow(abcd, IntPtr.Zero, "SysListView32", "");
+
+
+                    // do something with windowHandle
+                }
+
+                SystemWindow a = new SystemWindow(abcd1);
+
+
+                // sao = SystemAccessibleObject.FromPoint(4, 200);
+                try
+                {
+                    string marketwatch = abcd1.ToString();
+                    if (marketwatch == "0")
+                    {
+                    MessageBox.Show("Nest is not running or Market Watch not present check out and run real time combo again \n     thank you  ");
+
+                    }
+                    sao = SystemAccessibleObject.FromWindow(a, AccessibleObjectID.OBJID_WINDOW);
+                }
+                catch
+                {
+                    MessageBox.Show("Market Watch not found ");
+                    return;
+                }
+
+
+
+                f = sao.Children[3];
+
+
+
+                listBox1.Items.Clear();
+                for (int i = 0; i < 5;i++ )
+                {
+                    listBox1.Items.Add(f.Children[i].Name .ToString());
+                }
+
+
+            
+           
+
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            getsymbol();
+            
+            
             if(symbolname.Text=="")
             {
                 MessageBox.Show("Please Enter symbol name ");
